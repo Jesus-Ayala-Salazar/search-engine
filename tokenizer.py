f02b5df822b7c621c5bcade5113ab5ebb3ed30eb
Eduardo0301 <eduardomag27@yahoo.com>
2020-02-11 17:28:13 -0800

    Update tokenizer.py
    updated tokenizer that works with bs

--- tokenizer.py
+++ tokenizer.py
@@ -2,6 +2,7 @@ import os
 from bs4 import BeautifulSoup
 import lxml
 #tokenize --> use algorithms
+#dbdicto
 def tokenize_each_file(parent: str):
 	"""Given a directory open each file within the given corpus and tokenize"""
 	c = 0
@@ -13,17 +14,16 @@ def tokenize_each_file(parent: str):
 				#print(str(folde
 				with open(parent+"\\"+ folder+"\\" +file, "rb") as f:
 					html= f.read()
-					print(html)
-					soup = BeautifulSoup(f, "lxml")
+					#html = "<HTML>test<p>test2</p></HTML>"
+					soup = BeautifulSoup(html, "lxml")
+					
 					#print(folder, file)
-					print(soup.prettify())
-					break
+					#break
 				
-		break
-	print(str(soup.prettify()))
-	#soup.prettify()
-
-
+		#break
+	#print(str(soup.prettify()))
+"""tokenize
+INVIND[token] """
 if __name__ == "__main__":
 	tokenize_each_file("C:\\Users\\eduar\\121\\Assign3\\WEBPAGES_CLEAN")
 
