3fd2c1627dc7fef636fe55c658ab349df66e292b
Jesus-Ayala-Salazar <jaayala@uci.edu>
2020-02-12 02:20:53 -0800

    use json to read files

--- tokenizer.py
+++ tokenizer.py
@@ -3,44 +3,18 @@ import os
 import sys
 from bs4 import BeautifulSoup
 import lxml
-from nltk import word_tokenize
-import pymongo
+import json
 
-client = pymongo.MongoClient("mongodb+srv://admin:admincs121@cluster0-zsift.mongodb.net/test?retryWrites=true&w=majority")
-db = client.test
-SEdb = client["SEdb"]
-col = SEdb["InvInd"]
-print(db.list_collection_names())
 
-#tokenize --> use algorithms
-#dbdicto
-def tokenize_each_file(parent: str):
+def tokenize_each_file(filename: str):
 	"""Given a directory open each file within the given corpus and tokenize"""
-	c = 0
-	for folder in os.listdir(parent):
-		if folder.endswith(".json") or folder.endswith(".tsv"):
-			continue
-		else:
-			for file in os.listdir(parent+"\\"+folder):
-				with open(parent+"\\"+ folder+"\\" +file, "rb") as f:
-					html= f.read()
-					html = "<html>hello <p>world</html>"
-					soup = BeautifulSoup(html, "lxml")
+	with open(filename, 'r', encoding='utf8') as json_file:
+		data = json.load(json_file)
 
-					title = soup.title
-					h1 = soup.h1
-					h2 = soup.h2
-					h3 = soup.h3
-					h4 = soup.h4
-					body = soup.body.string
-					p = soup.p.string
-					print(body, "\n\n", p)
-				break
-		break
+		for path in data:
 
-"""tokenize
-INVIND[token] """
+
+
 if __name__ == "__main__":
-	print(client.list_database_names())
 	path = sys.argv[1]
 	tokenize_each_file(path)
