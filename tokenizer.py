9adc02b0bb1f6cda601244fd4cb29c35431fb109
LoyaFrancisco <Loyaa11@gmail.com>
2020-02-13 19:58:27 -0800

    Update tokenizer.py (i.e. attempted to filter out any unobtained text)
    +Attempted to get all of the text in the document if the word isn't part of the important tags (h1, h2 , bold, etc.). Couldn't do it unfortunately, but check it out anyway to see if y'all can get it
    +print out the lemmatized token list

--- tokenizer.py
+++ tokenizer.py
@@ -58,16 +58,25 @@ def tokenize_each_file(filename: str):
 
 		# need to get frequency in doc, doc_id, tags the token appears in
 		with open(html_filename, 'rb') as html_file:
-			#html_file = "<html><title>Hello World</title> <h1>This is it</h1></html>"
+			
+			#html_file = "<html><title>This is a title</title> <h1>This is h1</h1><b>bold 1</b>  <b>bold 2</b> <p>this is a <h1>header1</h1>paragraph <b>bolded in paragraph</b></p></html>"
 			soup = BeautifulSoup(html_file, 'lxml')
-			pageTitle = soup.title.text
-			pageH1 = soup.find_all('h1') 	#list
-			pageH2 = soup.find_all('h2')	#list
-			pageH3 = soup.find_all('h3') 	#list
-			pageH4 = soup.find_all('h4')	#list
-			pageH5 = soup.find_all('h5')	#list
-			pageH6 = soup.find_all('h6')	#list
-			print(pageH1)
+			
+			importantTags = {'h1','h2','h3','h4','h5','h6', 'b','strong'}
+			#pageTitle = soup.title.text
+			pageH1 = list(word.text for word in soup.find_all('h1')) 	#list
+			pageH2 = list(word.text for word in soup.find_all('h2'))	#list
+			pageH3 = list(word.text for word in soup.find_all('h3')) 	#list
+			pageH4 = list(word.text for word in soup.find_all('h4'))	#list
+			pageH5 = list(word.text for word in soup.find_all('h5'))	#list
+			pageH6 = list(word.text for word in soup.find_all('h6'))	#list
+			pageBold = list(word.text for word in soup.find_all('b'))	#list
+			pageStrong = list(word.text for word in soup.find_all('s'))	#list
+
+			###This is me attempting to get all of the text in the document if the word isn't part of any of the ALREADY GOTTEN tags.###
+			# wordList = soup.find_all(lambda tag: tag.name not in importantTags and tag.name not in ['title','html','head','body'])
+			# wordList = list(word.text for word in wordList)
+			# print(wordList)
 
 			# tokenize
 			token_list = nltk.word_tokenize(soup.get_text())
@@ -76,12 +85,13 @@ def tokenize_each_file(filename: str):
 			# lemmatize
 			lemmatizer = WordNetLemmatizer()
 			#token_list = [lemmatizer.lemmatize(token) for token in token_list]
-			token_list = list()
+			lemmatized_token_list = list()
 			tokenFrequency = defaultdict(int)
 			for token in token_list:
 				changedToken = lemmatizer.lemmatize(token)
 				tokenFrequency[changedToken] += 1
-				token_list.append(changedToken)
+				lemmatized_token_list.append(changedToken)
+			print(lemmatized_token_list)
 
 
 		break
