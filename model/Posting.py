7154edc93c4d791940f6f4555dee1b21afd59e9b
Jesus-Ayala-Salazar <jaayala@uci.edu>
2020-02-28 18:08:00 -0800

    fuck

--- model/Posting.py
+++ model/Posting.py
@@ -7,23 +7,15 @@ class Posting:
 		#if only one arg is given it is the doc_id
 		if len(args) == 1:
 			self.doc_id = args[0]
-			self.freq = 0
 			self.tags = defaultdict(int)    # {tag: freq}
 			self.tf_idf = 0
+			self.weight = 0
 		else: # if not one, then it must be 4, and in order it is doc_id,freq,tags,tf_idf
 			self.doc_id = args[0]
 			self.freq = args[1]
 			self.tags = args[2]
 			self.tf_idf = args[3]
-	
-	def get_doc_id(self):
-		return self.doc_id
-	def get_freq(self):
-		return self.freq
-	def get_tags(self):
-		return self.tags
-	def get_tf_idf(self):
-		return self.tf_idf
+
 	# for debugging
 	def __str__(self):
 		return f'doc id: {self.doc_id} | freq: {self.freq} | tags: {self.tags} | tf_idf: {self.tf_idf}'
